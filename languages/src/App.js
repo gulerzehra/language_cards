9011cb92e2f0b44cc48ce7a4f714933f77364dd9
gulerzehra <zehraaguleer08@gmail.com>
2022-11-03 22:00:39 +0300

    temel1

--- languages/src/App.js
+++ languages/src/App.js
@@ -1,8 +1,13 @@
+import Card from "./companents/Card";
+import Header from "./companents/Header";
+import Container from "react-bootstrap/Container";
+
 function App() {
   return (
-    <div className="App">
-      <p>hello react</p>
-    </div>
+    <Container className="text-center mt-4">
+      <Header />
+      <Card />
+    </Container>
   );
 }
 
---
+++ languages/src/companents/Language.jsx
@@ -0,0 +1,7 @@
+import { Container } from "react-bootstrap/Container";
+
+const Language = () => {
+  return <div>Language</div>;
+};
+
+export default Language;
