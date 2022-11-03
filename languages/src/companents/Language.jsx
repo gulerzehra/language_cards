28283b463df43937d984d3ae047e673c23e47e2a
gulerzehra <zehraaguleer08@gmail.com>
2022-11-04 00:06:04 +0300

    almost

--- languages/src/companents/Language.jsx
+++ languages/src/companents/Language.jsx
@@ -1,5 +1,14 @@
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
 const Language = ({ name, img, options }) => {
-  return <div>Language</div>;
+  return (
+    <Container className="p-4 rounded-2" style={{ background: "peachpuff" }}>
+      <Container>
+        <Image src={img} width="70%"></Image>
+        <h3 className="display-6">{name}</h3>
+      </Container>
+    </Container>
+  );
 };
 
 export default Language;
